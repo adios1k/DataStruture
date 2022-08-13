3323ff571eeb24af6ebbc2ec09887bc0bf32a8cb
Kowshik chakraborty <Kowshik.Chakraborty@thoughtworks.com>
2022-08-14 01:41:33 +0530

    1t class

--- src/SinglyLinkedList.java
+++ src/SinglyLinkedList.java
@@ -10,4 +10,18 @@ public class SinglyLinkedList {
             this.next = null;
         }
     }
+
+    public static void main (String[] args){
+        SinglyLinkedList sll = new SinglyLinkedList();
+        sll.head = new ListNode(10);
+        ListNode second = new ListNode(1);
+        ListNode third = new ListNode(8);
+        ListNode fourth = new ListNode(11);
+
+        // Now we will connect them together to form a chain
+        sll.head.next= second;
+        second.next=third;
+        third.next= fourth;
+
+    }
 }
